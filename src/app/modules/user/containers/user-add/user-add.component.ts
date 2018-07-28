6281bb74943d328823a36be65854706f5529dd88
Michael Cohen <michael.cohen@buildasign.com>
2018-07-28 11:03:36 -0500

    initial setup of add user form

--- src/app/modules/user/containers/user-add/user-add.component.ts
+++ src/app/modules/user/containers/user-add/user-add.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-add',
   templateUrl: './user-add.component.html',
-  styleUrls: ['./user-add.component.css']
+  styleUrls: ['./user-add.component.scss']
 })
-export class UserAddComponent implements OnInit {
+export class UserAddComponent {
+  options;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public dialogRef: MatDialogRef<UserAddComponent>,
+    private fb: FormBuilder
+  ) {
+    this.options = this.fb.group({
+      hideRequired: false,
+      floatLabel: 'auto',
+      name: new FormControl(''),
+      age: new FormControl(''),
+      weight: new FormControl('')
+    });
   }
-
+  addUser() {
+    if(this.options.valid) {
+      console.log('adding user', this.options);
+      this.dialogRef.close(this.options.value);
+    }
+  }
 }
