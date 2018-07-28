9dfda661b298e3bee854873953aeaf46a77f84cc
Michael Cohen <michael.cohen@buildasign.com>
2018-07-27 21:43:40 -0500

    junk test

--- src/app/modules/user/components/user-list/user-list.component.spec.ts
+++ src/app/modules/user/components/user-list/user-list.component.spec.ts
@@ -8,13 +8,16 @@ describe('UserListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [
+        UserListComponent,
+        HostComponent
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserListComponent);
+    fixture = TestBed.createComponent(HostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -22,4 +25,61 @@ describe('UserListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should display a list of users as long as the input', () => {
+
+  });
+  it('should display the number of friends a user has', () => {
+
+  });
+  it('should display the user\'s age', () => {
+
+  });
+  it('should display the user\'s weight', () => {
+
+  });
+  it('should emit an event when clicked', () => {
+
+  });
 });
+
+import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { UserState } from '../../store';
+
+@Component({
+  selector: 'test-host',
+  template: '<app-user-list [users]="users"></app-user-list>'
+})
+
+export class HostComponent {
+  users: Observable<UserState>;
+  constructor() {
+    this.users = of({
+      ids: ['1', '5', '7'],
+      entities: {
+        1: {
+          id: '1',
+          weight: 180,
+          age: 32,
+          name: 'Franklin',
+          friends: ['5']
+        },
+        5: {
+          id: '5',
+          weight: 210,
+          age: 42,
+          name: 'Biff',
+          friends: ['1', '5', '7']
+        },
+        7: {
+          id: '7',
+          weight: 165,
+          age: 21,
+          name: 'Ralph',
+          friends: ['5', '1']
+        }
+      }
+    })
+  }
+}
