a24ad699e5649c88c44e9607ff757f0e90c29599
Michael Cohen <michael.cohen@buildasign.com>
2018-07-28 16:13:44 -0500

    add router links

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,17 +1,19 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
+import { Routes, RouterModule } from '@angular/router';
 import { MatInputModule } from '@angular/material';
 import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { StoreModule } from '@ngrx/store';
 
 import * as fromUser from './store/user.reducer';
 import { UserListComponent } from './components';
 import { UserAddComponent } from './containers';
 import { UserService } from './user.service';
 import { SharedModule } from '../../shared/shared.module';
+import { UserDetailsComponent } from './containers/user-details/user-details.component';
 
 const COMPONENTS = [
   UserListComponent,
@@ -26,15 +28,22 @@ const MATERIAL_MODULES = [
   MatCheckboxModule
 ];
 
+
+const routes: Routes = [
+  { path: 'user/:id', component: UserDetailsComponent },
+];
+
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule.forChild(routes),
     ...MATERIAL_MODULES,
     SharedModule,
     StoreModule.forFeature('user', fromUser.reducer)
   ],
   declarations: [
-    ...COMPONENTS
+    ...COMPONENTS,
+    UserDetailsComponent
   ],
   exports: [
     ...COMPONENTS
