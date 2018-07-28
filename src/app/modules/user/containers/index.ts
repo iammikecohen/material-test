e90f240874c23bc30a81aaa61e30817179775ee2
Michael Cohen <michael.cohen@buildasign.com>
2018-07-28 16:13:33 -0500

    cleanup

--- src/app/modules/user/containers/index.ts
+++ src/app/modules/user/containers/index.ts
@@ -1,1 +1,2 @@
 export * from './user-add/user-add.component';
+export * from './user-details/user-details.component';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
@@ -10,12 +11,14 @@ import { MatIconModule } from '@angular/material/icon';
     FormsModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatToolbarModule,
     ReactiveFormsModule
   ],
   exports: [
     FormsModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatToolbarModule,
     ReactiveFormsModule
   ],
   declarations: []
